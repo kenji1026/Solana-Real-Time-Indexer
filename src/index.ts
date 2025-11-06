5a59890dce9645715c16f3a71c0e9315267f4356
cheeming <dragon.steal75@gmail.com>
2025-11-06 18:23:48 +0900

    add main process

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,20 @@
 import * as dotenv from 'dotenv';
+import { YellowstoneClient } from './grpc/client';
+import { createTokenSubscription } from './grpc/subscriptions';
 
 dotenv.config();
 
-async function main() {}
+async function main() {
+  try {
+    const client = new YellowstoneClient();
+    const subscription = createTokenSubscription([]);
+
+    await client.subscribe(subscription);
+  } catch (error) {
+    console.error('Fatal error starting indexer', error);
+    process.exit(1);
+  }
+}
 
 process.on('SIGINT', async () => {
   try {
